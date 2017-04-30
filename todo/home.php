c07886300161e6565764f9cc4890f6d3b4737433
ankan basu <ab936@afsconnect1.njit.edu>
2017-04-30 19:03:57 -0400

    basic home page after login

---
+++ todo/home.php
@@ -0,0 +1,13 @@
+<?php include '../view/fp_header.php'; ?>
+<main>
+    <h4>Welcome...</h4>
+    <a href="#List of incomplete items">List of incomplete items</a><br>
+    <a href="#List of complete items">List of complete items</a><br><br>
+
+    <form action="../index.php" method="post" name="logout_form">
+    <input type="hidden" name="action" value="user_logout">
+        <label>&nbsp;</label>
+	<input type="submit" value="Logout" />
+    </form><br><br>
+</main>
+<?php include '../view/fp_footer.php'; ?>
