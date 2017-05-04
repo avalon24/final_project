2f584f355b9853909efb9701cc62d4ee4ef3999b
avalon24 <ankan.basu85@gmail.com>
2017-05-04 06:02:43 -0400

    modifying error page

--- errors/error.php
+++ errors/error.php
@@ -2,6 +2,6 @@
 <div id="main">
     <h1 class="top">Error</h1>
     <p class="first_paragraph"><?php echo $error; ?></p>
-    <a href="./login.php">Go Back to Login Page</a>
+    <a href="../login.php">Go Back to Login Page</a>
 </div>
 <?php include 'view/fp_footer.php'; ?>
