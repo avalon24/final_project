b6df2a020b68687d761242c922887168c4e8ff33
ankan basu <ab936@afsconnect1.njit.edu>
2017-05-03 20:23:47 -0400

    routing to login page

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
+require('database.php');
+require('db_function.php');
+
+    $action=filter_input(INSERT_POST,"action");
+    if($action == NULL) {
+        $action = "view_login";
+    }
+    if($action == "view_login") {
+        include ('login.php');
+    }
 
 ?>
