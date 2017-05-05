ff74d9cd6a32233099a49313435a56c2bec2ea46
avalon24 <ankan.basu85@gmail.com>
2017-05-05 00:59:49 -0400

    changes to reuse same function

--- model/todo_function.php
+++ model/todo_function.php
@@ -1,12 +1,13 @@
 <?php
 
-function getPendingItems($user_id) {
+function getPendingItems($user_id,$status) {
     global $db;
     $query='select * from fp_todo_list
             where t_u_id = :user_id and
-	          t_status = "P"';
+	          t_status = :status';
     $statement=$db->prepare($query);
     $statement->bindValue(':user_id',$user_id);
+    $statement->bindValue(':status',$status);
     $statement->execute();
     $result=$statement->fetchAll();
     $statement->closeCursor();
