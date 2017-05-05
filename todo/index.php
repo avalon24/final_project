731cd7db8bfc9f3c5df98166591093a36bd8f010
avalon24 <ankan.basu85@gmail.com>
2017-05-05 00:56:28 -0400

    changes to reuse same function

--- todo/index.php
+++ todo/index.php
@@ -9,10 +9,12 @@ require('../model/todo_function.php');
     } else if($action == "pwd_change") {
         include ('pwd_change.php');
     } else if($action == "pending_items") {
-        $result=getPendingItems($_COOKIE['user_id']);
+        echo "current value of uid = "; echo $_COOKIE['user_id']; echo "###";
+        $result=getTodoItems($_COOKIE['user_id'],"P");
+	echo $result[0]['t_date'];
 	include ('pending_items.php');
     } else if($action == "finished_items") {
-        $result=getFinishedItems($_COOKIE['user_id']);
+        $result=getTodoItems($_COOKIE['user_id'],"C");
 	include ('finished_items.php');
     }
 
