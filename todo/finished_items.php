1a0e4b80c965b7e402558d9eb6506f49c0703dab
avalon24 <ankan.basu85@gmail.com>
2017-05-05 06:47:42 -0400

    added delete feature

--- todo/finished_items.php
+++ todo/finished_items.php
@@ -13,13 +13,20 @@
             <td><i><?php echo $finished['t_date'];?></i></td>
             <td><i><?php echo $finished['t_time'];?></i></td>
 	    <td><i><?php echo $finished['t_desc'];?></i></td>
+	    <td><form action="index.php" method="post">
+	        <input type="hidden" name="action" value="delete_item">
+	            <input type="hidden" name="item_id" value="<?php echo $pending['t_id'] ?>"/> 
+		    <input type="hidden" name="pg_val" value="P" />
+		    <input type="submit" value="Delete">
+		</form></td>
+
 	</tr>
 	<?php } ?>
-	</table>
+	</table><br><br>
 	
 	<form action='home.php' method="post">
 	    <input type="submit" value="Go back">
-	</form>
+	</form><br>
 	
 </main>
 <?php include '../view/fp_footer.php' ?>
