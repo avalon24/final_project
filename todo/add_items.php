db828dfa18b7c35514f742b205a3931cb1a4624a
avalon24 <ankan.basu85@gmail.com>
2017-05-05 04:20:37 -0400

    added provision to return back and message option

--- todo/add_items.php
+++ todo/add_items.php
@@ -2,6 +2,7 @@
 <main>
 
     <h1>Add new items</h1>
+    <h3><?php echo $message ?></h3>
     <h4>Hi <?php echo $_COOKIE['login'] ?>, to add an item please enter the following values - </h4>
 
     <form action="index.php" method="post" id="add_items_form">
@@ -17,7 +18,14 @@
 	<input type="text" name="item_desc" ><br><br><br>
         
 	<label>&nbsp;</label>
-	<input type="submit" value="Submit"/><br>
+	<input type="submit" value="Submit"/><br><br>
+	
+    </form>
+    
+    <form action="index.php" method="post">
+    <input type="hidden" name="action" value="pending_items">
+        <label>&nbsp;</label>
+	<input type="submit" value="Go Back"><br><br>
 
     </form>
 
