f720b54e4a914214cb26b2a2e00d968f78039ff5
avalon24 <ankan.basu85@gmail.com>
2017-05-05 07:11:58 -0400

    using ORDER BY arranged chronologically

--- model/todo_function.php
+++ model/todo_function.php
@@ -4,7 +4,8 @@ function getTodoItems($user_id,$status) {
     global $db;
     $query='select * from fp_todo_list
             where t_u_id = :user_id and
-	          t_status = :status';
+	          t_status = :status
+            order by t_date, t_time asc';
     $statement=$db->prepare($query);
     $statement->bindValue(':user_id',$user_id);
     $statement->bindValue(':status',$status);
