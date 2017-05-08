52e062cc3b2319e5667dfc59cd57f1f625d23887
avalon24 <ankan.basu85@gmail.com>
2017-05-08 19:07:51 -0400

    final positioning changes

--- logout.php
+++ logout.php
@@ -1,7 +1,9 @@
 <?php include 'view/fp_header.php'; ?>
 <main>
-    <h1>Congratulations!!!</h1><br>
-        <h4><center>You have successfully logged out of the system...</center></h4><br><br>
-    <a href="login.php">Click here, if you wish to login again!</a><br><br> 
+<div class="tab">
+    <h1>&nbsp;&nbsp;&nbsp;&nbsp;Congratulations!!!</h1><br>
+        <h4><center><i>You have successfully logged out of the system...</i></center></h4><br><br>
+    <center><a href="login.php">Click here, if you wish to login again!</a></center><br><br> 
+</div>
 </main>
 <?php include 'view/fp_footer.php'; ?>
