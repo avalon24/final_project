a80e9a5d278fac1f45b79a294cea5a3e0dce6e1a
avalon24 <ankan.basu85@gmail.com>
2017-05-08 18:44:01 -0400

    final positioning changes

--- pwd_reset_nxt.php
+++ pwd_reset_nxt.php
@@ -1,28 +1,30 @@
 <?php include 'view/fp_header.php'; ?>
 <main>
-    <h1>Reset password!!</h1>
-    <h3><?php echo $message ; ?></h3>
+<div class="tab">
+    <h1>&nbsp;&nbsp;&nbsp;&nbsp;Reset password!!</h1>
+    <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $message ; ?></h3>
     
-    <form action="index.php" method="post" id="reset_form">
+    <form action="index.php" method="post" align="center">
     <input type=hidden name="action" value="pwd_reset">
         
 	<input type="hidden" name="userid" value="<?php echo $result[0]['userid']; ?>" />
         <input type="hidden" name="uname"  value="<?php echo $result[0]['uname'];  ?>" />
 
-	<label>Question: <?php echo $result[0]['resetq']; ?></label><br><br>
+	<label><i>Question: <?php echo $result[0]['resetq']; ?></i></label><br><br>
 
-        <label>Answer: </label>
+        <label><i>Answer:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i> </label>
 	<input type="text" name="reseta" placeholder="Your answer" /><br><br><br>
 
-	<label>New password:</label>
+	<label><i>New password:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i></label>
 	<input type="password" name="password"><br><br>
 
-	<label>Retype password: </label>
+	<label><i>Retype password:&nbsp;&nbsp;&nbsp;&nbsp;</i></label>
 	<input type="password" name="pass_temp"><br><br>
 	
 	<label>&nbsp;</label>
 	<input type="submit" value="Submit" /><br><br>
     
     </form>
+</div>
 </main>
 <?php include 'view/fp_footer.php'; ?>
