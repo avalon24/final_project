53f432ce26a17abd053bfbe332405b49b50f5911
avalon24 <ankan.basu85@gmail.com>
2017-05-08 17:20:18 -0400

    final positioning changes

--- todo/change_pwd.php
+++ todo/change_pwd.php
@@ -1,18 +1,19 @@
 <?php include '../view/fp_header.php'; ?>
 <main>
-    <h1>Change password!!</h1>
-    <h3><i><?php echo $message; ?></i></h3>
-    <h4><i>Hi <?php echo $_COOKIE['login'] ?>, please update:</i></h4>    
+<div class="tab">    
+    <h1>&nbsp;&nbsp;&nbsp;&nbsp;Change password!!</h1>
+    <h3><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $message; ?></i></h3>
+    <h4><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Hi <?php echo $_COOKIE['login'] ?>, please update:</i></h4>    
     <form action="index.php" method="post" id="reset_form">
     <input type=hidden name="action" value="change_pwd">
         
 	<input type="hidden" name="userid" value="<?php echo $_COOKIE['userid']; ?>" />
 	<input type="hidden" name="reseta" value="<?php echo $_COOKIE['secret_ans']; ?>" />
 
-	<label>New password:</label>
+	<label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New password:</label>
 	<input type="password" name="password"><br><br>
 
-	<label>Retype password: </label>
+	<label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Retype password: </label>
 	<input type="password" name="pass_temp"><br><br>
 	
 	<label>&nbsp;</label>
@@ -24,6 +25,6 @@
        	<label>&nbsp;&nbsp;&nbsp;&nbsp;</label>
         <input type="submit" value="Go back"><br><br><br>
     </form>
-
+</div>
 </main>
 <?php include '../view/fp_footer.php'; ?>
