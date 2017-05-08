e5501a1c786a71859b80967dbd4bb7f6d66f7a6a
avalon24 <ankan.basu85@gmail.com>
2017-05-08 19:03:29 -0400

    final positioning changes

--- login.php
+++ login.php
@@ -2,8 +2,8 @@
 <main>
 <div class="tab">
     <h1>&nbsp;&nbsp;&nbsp;&nbsp;Login Page!!</h1>
-    <h3><?php echo $message ?></h3>
-    <h4><?php echo $success ?></h4>
+    <h3><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $message ?></i></h3>
+    <h4><i><center><?php echo $success ?><center></i></h4>
     <form action="index.php" method="post" id="login_form" align="right">
         <input type=hidden name="action" value="user_login">
 
@@ -17,7 +17,7 @@
 
 	<label>&nbsp;</label>
 	<input type="submit" value="Login" />
-	<br><br>
+	<br><br><br><br>
     </form>
 
     <center><a href="register.php">New user</a>  |  <a href="pwd_reset.php">Forgot Password</a><br><br></center>
