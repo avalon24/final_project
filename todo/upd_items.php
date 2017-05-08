7d715853019422fd088c0d7fe528dadd6888cd78
avalon24 <ankan.basu85@gmail.com>
2017-05-08 17:52:05 -0400

    final positioning changes

--- todo/upd_items.php
+++ todo/upd_items.php
@@ -1,6 +1,8 @@
 <?php include '../view/fp_header.php' ?>
 <main>
-    <h4>Hello <?php echo $_COOKIE['login'] ?>, the task you wish to update is:</h4>
+<div class="tab">
+    <h4><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Hello <?php echo $_COOKIE['login'] ?>, the task you wish to update
+    is:</i></h4>
     
     <?php $itemid=filter_input(INPUT_POST,'item_id');
           $itemdt=filter_input(INPUT_POST,'item_dt');
@@ -8,7 +10,7 @@
 	  $idesc=filter_input(INPUT_POST,'item_desc');
     ?>
 
-    <table>
+    <table align="center">
         <tr>
             <td><b>Due Date<b></td>
             <td><b>Due Time<b></td>
@@ -20,29 +22,30 @@
             <td><i><?php echo $itemtm; ?></i></td>
             <td><i><?php echo $idesc;  ?></i></td>
         </tr><br><br>
-    </table>
+    </table><br>
 
-    <form action="index.php" method="post">
+    <form action="index.php" method="post" id="reset_form">
     <input type="hidden" name="action" value="update_item">
         <input type="hidden" name="item_id" value="<?php echo $itemid ?>" />
         <input type="hidden" name="old_date" value="<?php echo $itemdt ?>" />
 	<input type="hidden" name="old_time" value="<?php echo $itemtm ?>" />
 
-        <h4>Fill in the data you wish to change - </h4>
-        <label>New date: </label>
+        <h4><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fill in the data you wish to change - </i></h4>
+        <label><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New date: </i></label>
         <input type="date" name="new_date" value="<?php echo $pending['t_date'] ?>" /><br><br>
 
-        <label>New time: </label>
+        <label><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New time: </i></label>
         <input type="time" name="new_time" value="<?php echo $pending['t_time'] ?>" /><br><br>
 
         <label>&nbsp;</label>
         <input type="submit" value="Done"><br><br>
     </form>
 
-    <form action="index.php" method="post">
+    <form action="index.php" method="post" id="reset_form">
     <input type="hidden" name="action" value="cancel_upd">
+    	<label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
         <input type="submit" value="Cancel Update">
     </form><br><br>
-
+</div>
 </main>
 <?php include '../view/fp_footer.php' ?>
