f1f806f583eeeb6a94e599fb65bdec4c97bb89d3
avalon24 <ankan.basu85@gmail.com>
2017-05-08 14:22:28 -0400

    removing type=submit with button, adding div styling

--- todo/home.php
+++ todo/home.php
@@ -1,35 +1,38 @@
 <?php include '../view/fp_header.php'; ?>
 <main>
+    
+    <div class="tab">
     <h4>Welcome...<?php echo $_COOKIE['login']; echo $success; ?></h4><br><br>
+
+    <form action="home.php" method="post" name="upd_prof_form">
+    <input type="hidden" name="action">
+        <button class="tablinks">Home</button>
+    </form>
 
     <form action="upd_profile.php" method="post" name="upd_prof_form">
     <input type="hidden" name="action" value="upd_profile" >
-        <label><i># Update your profile data</i></label>
-	<input type=submit value="Go">
-    </form><br><br>
+	<button class="tablinks">Update your Profile</button>
+    </form>
 
     <form action="change_pwd.php" method="post" name="pwd_chng_form">
     <input type="hidden" name="action" value="pwd_change" >
-        <label><i># Change Password</i></label>
-	<input type="submit" value="Go">
-    </form><br><br>
+	<button class="tablinks">Change Password</button>
+    </form>
 
     <form action="index.php" method="post" name="pending_form">
     <input type="hidden" name="action" value="pending_items">
-        <label><i># List of pending items</i></label>
-	<input type="submit" value="Go">
-    </form><br><br>
+	<button class="tablinks">Pending Tasks</button>
+    </form>
 
     <form action="index.php" method="post" name="finished_form">
     <input type="hidden" name="action" value="finished_items" >
-        <label><i># List of finished items</i></label>
-	<input type="submit" value="Go">
-    </form><br><br><br><br>
+	<button class="tablinks">Finished Tasks</button>
+    </form>
 
     <form action="../index.php" method="post" name="logout_form">
     <input type="hidden" name="action" value="user_logout">
-        <label>&nbsp;</label>
-	<input type="submit" value="Logout" />
+	<button class="tablinks">Logout</button>
     </form><br><br>
+    </div>
 </main>
 <?php include '../view/fp_footer.php'; ?>
