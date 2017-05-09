18dce5dcb705069543da252525011476fd94859b
avalon24 <ankan.basu85@gmail.com>
2017-05-08 20:27:37 -0400

    final positioning changes

--- register.php
+++ register.php
@@ -1,43 +1,43 @@
 <?php include 'view/fp_header.php';?>
 <main>
-    <h3><?php echo $message ?></h3>
-    <h5><i> * marked fields are mandatory!</i><h5>
-    <h4>Enter your personal details below:</h4>
-    <form action="index.php" method="post" id="register_form">
-        <input type="hidden" name="action" value="new_user">
-
-	<label>*Given Name: </label>
+<div class="reg">
+    <h3><i>&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $message ?></i></h3>
+    <h5><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;* marked fields are mandatory!</i><h5>
+    <h4><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Enter your personal details below:</i> </h4>
+    <form action="index.php" method="post" id="reg_form" >
+    <input type="hidden" name="action" value="new_user">
+	<label align="right"><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*Given Name: </i></label>	
 	<input type="text" name="f_name" placeholder="Your name" /><br>
 
-        <label>*Last Name: </label>
+        <label align="right"><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*Last Name: </i></label>
         <input type="text" name="l_name" placeholder="Last name" /><br><br>
 
-        <label> Phone No: </label>
+        <label align="right"><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Phone No: </i></label>
         <input type="text" name="contact" placeholder="Enter 10 digits only" ><br><br>
 
-        <label>*Date of Birth: </label>
+        <label align="right"><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*Date of Birth: </i></label>
         <input type="date" name="dob" /><br>
 
-        <label>*Gender: </label>
+        <label align="right"><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*Gender: </i></label>
         <select name="gender" >
             <option value="Female">Female
             <option value="Male">Male
             <option value="Other">Other
         </select><br>
 									     
-        <h4>Following information will be used as your <b>Login</b> credentials</h4>
+        <h4><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Following information will be used as your <b>Login</b> credentials</i></h4>
 
-        <label>*Email ID: </label>
+        <label align="right"><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*Email ID: </i></label>
         <input type="text" name="email" placeholder="Valid email address" /><br>
 
-        <label>*New Password: </label>
+        <label align="right"><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*New Password: </i></label>
 	<input type="password" name="password"><br>
 
-	<label>*Reenter Password: </label>
+	<label align="right"><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*Retype Password: </i></label>
 	<input type="text" name="pass_temp"><br>
 
-        <h4>Select a secret question from below for password reset</h4>
-	<label>*Question: </label>
+        <h4><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Select a secret question from below for password reset</i></h4>
+	<label align="right"><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*Question: </i></label>
 	<select name="reset_ques" >
             <option value="1">What is your favorite color?
 	    <option value="2">What is your mother's maiden name?
@@ -45,12 +45,12 @@
 	    <option value="4">What is your favorite cuisine?
 	</select><br>
 
-	<label>*Answer:</label>
+	<label align="right"><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*Answer: </i></label>
 	<input type="text" name="reset_ans" /><br><br>
 
 	<label>&nbsp; </label>
-        <input type="submit" value="submit">
+        <input type="submit" value="submit"><br><br>
     </form>
-
+</div>
 </main>
 <?php include 'view/fp_footer.php'; ?>
